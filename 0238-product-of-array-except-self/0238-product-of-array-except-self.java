eb36547a4397fdadde648eee67794f2209fc9882
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-02-01 13:54:01 +0530

    Time: 2 ms (94.22%), Space: 72.5 MB (12.98%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        for(int i =0;i<n;i++){
+          output[i]=1;
+        }
+        int left=1;
+        for(int i=0;i<n;i++){
+            output[i]*=left;
+            left*=nums[i];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
+    }
+}
