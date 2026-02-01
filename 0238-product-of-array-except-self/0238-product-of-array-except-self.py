e1aff03265722fb3a1018c609658837d1261a171
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-02-01 13:58:48 +0530

    Time: 23 ms (60.36%), Space: 25.1 MB (59.13%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        output =[1]*n
+        left =1
+        for i in range(n):
+            output[i]*=left
+            left*=nums[i]
+        right =1
+        for i in range(n-1,-1,-1):
+            output[i]*=right
+            right*=nums[i]
+        return output
