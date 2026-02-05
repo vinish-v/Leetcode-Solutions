951210984361b9eb198aff6902d9405a3fca53f8
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-02-05 10:49:17 +0530

    Time: 1 ms (99.73%), Space: 42.4 MB (89.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        long reverse = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = (reverse * 10) + digit;
+            x = x / 10;
+        }
+        if(reverse>Integer.MAX_VALUE||reverse<Integer.MIN_VALUE) return 0;
+        else return (int)reverse;
+    }
+}
