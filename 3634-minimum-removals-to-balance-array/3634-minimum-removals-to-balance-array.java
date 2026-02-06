859ebe66f629ab1641192c6d9ed96af81327946b
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-02-06 09:48:39 +0530

    Time: 25 ms (58.63%), Space: 85.9 MB (92.77%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -1,18 +1,14 @@
 class Solution {
     public int minRemoval(int[] nums, int k) {
         Arrays.sort(nums);
-        int n = nums.length;
         int left =0;
         int maxlen =0;
-        for(int right =0 ; right<n;right++){
-            while((long)nums[right] >(long) nums[left]*k){
+        for(int right =0;right <nums.length;right++ ){
+            while( (long)nums[right] > (long) nums[left]*k){
                 left++;
             }
-            maxlen =Math.max(maxlen,right-left +1);
-
-
+            maxlen = Math.max(maxlen , right -left +1);
         }
-        return n -maxlen;
-
+        return nums.length - maxlen ;
     }
 }
