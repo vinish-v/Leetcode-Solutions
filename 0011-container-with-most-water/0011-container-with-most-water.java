4afce6cdf8a0870bc4cbec95c8684d485aa28f5b
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-02-07 08:46:25 +0530

    Time: 5 ms (79.84%), Space: 77.3 MB (65.9%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -18,3 +18,33 @@ class Solution {
         return maxlen;
     }
 }
+/*
+import java.util.*;
+
+class Solution {
+    public int secondMaxSquare(int[] nums) {
+        // Step 1 & 2: remove duplicates using Set
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        // Convert back to list
+        List<Integer> list = new ArrayList<>(set);
+
+        // Step 3: sort
+        Collections.sort(list);
+
+        // Edge case: need at least 2 distinct elements
+        if (list.size() < 2) {
+            return -1; // or throw exception
+        }
+
+        int secondMax = list.get(list.size() - 2);
+
+        // Step 4: square and return
+        return secondMax * secondMax;
+    }
+}
+*/
+
