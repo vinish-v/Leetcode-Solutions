97f78d3a03d855506040aa812bed09416ea33c8b
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-02-08 21:57:35 +0530

    Time: 31 ms (80.23%), Space: 59.2 MB (57.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,34 +3,33 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
-
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        int i =0;
+        for(i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-
-            int low = i + 1;
-            int high = n - 1;
-
-            while (low < high) {
-                int sum = nums[i] + nums[low] + nums[high];
-
-                if (sum > 0) {
-                    high--;
-                } else if (sum < 0) {
-                    low++;
-                } else {
-                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                    low++;
-                    high--;
-                    while (low < high && nums[low] == nums[low - 1]) {
-                        low++;
-                    }
-                    while (low < high && nums[high] == nums[high + 1]) {
-                        high--;
-                    }
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] +nums[right];
+                if(sum >0){
+                    right --;
                 }
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    result.add(Arrays.asList(nums[i] , nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left-1]){
+                        left++;
+                    }
+                    while(left<right && nums[right] == nums[right+1]){
+                        right--;
+                    }
+
+                } 
             }
         }
         return result;
