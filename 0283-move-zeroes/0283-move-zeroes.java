abe7f58312da752f7cc324266513405dd0c65c5c
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-02-08 20:29:18 +0530

    Time: 2 ms (90.05%), Space: 48.1 MB (16.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left =0;
-        for(int right=0;right<nums.length;right++){
-            if(nums[right] != 0){
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
+        int i =0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] !=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] =temp;
+                i++;
             }
         }
     
