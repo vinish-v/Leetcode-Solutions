beffa1c962e0a626a493446b597386900eda68ae
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-03-09 09:44:31 +0530

    Time: 1 ms (15.03%), Space: 44.3 MB (97.05%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int  n = accounts.length;
+        int max = Integer.MIN_VALUE;
+        for(int i =0;i<n;i++){
+            int sum =0;
+            int m = accounts[i].length;
+            for(int j =0;j<m;j++){
+                sum+=accounts[i][j];
+                max = Math.max(max,sum);
+            }
+
+
+        }
+        return max;
+    }
+}
