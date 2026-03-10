7a4b9cef8718f67b4c83a086b5586760ea3ebb0c
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-03-10 21:56:26 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.82%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String maximumTime(String time) {
+        char[] t = time.toCharArray();
+
+        if (t[0] == '?') {
+            t[0] = (t[1] != '?' && t[1] > '3') ? '1' : '2';
+        }
+
+        if (t[1] == '?') {
+            t[1] = (t[0] == '2') ? '3' : '9';
+        }
+
+        if (t[3] == '?') {
+            t[3] = '5';
+        }
+
+        if (t[4] == '?') {
+            t[4] = '9';
+        }
+
+        return new String(t);
+    }
+}
